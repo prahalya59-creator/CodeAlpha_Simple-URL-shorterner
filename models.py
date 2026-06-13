7682176198e2c63f0506cdf35f37a0cebad314a7
prahalya59-creator <prahalya69@gmail.com>
2026-06-13 12:50:11 +0530

    Update models.py

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class URL(db.Model):
 
     id = db.Column(
@@ -15,7 +16,12 @@ class URL(db.Model):
     )
 
     short_code = db.Column(
-        db.String(10),
+        db.String(50),
         unique=True,
         nullable=False
+    )
+
+    clicks = db.Column(
+        db.Integer,
+        default=0
     )
